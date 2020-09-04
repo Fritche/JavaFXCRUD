6286b9e85c3702a6306d4da5c6fc1b0e87c6d882
Fritche <fritche@gmail.com>
2020-09-04 11:19:54 -0300

    DAO javafx

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,15 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entidades.Department;
+import model.entidades.Seller;
+
+public interface SellerDao {
+	void insert(Seller obj);
+	void update(Seller obj);
+	void deleteById(Integer id);
+	Seller findById(Integer id);
+	List<Seller> findByDepartment(Department department);
+	List<Seller> findAll();	
+}
